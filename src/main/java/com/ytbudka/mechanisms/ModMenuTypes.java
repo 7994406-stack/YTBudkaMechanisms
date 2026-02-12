95a6543c6b539b3d2898688700919ffae05330ce
7994406-stack <7994406@gmail.com>
2026-02-12 18:57:02 +0300

    Fix ModMenuTypes.java file corruption

--- src/main/java/com/ytbudka/mechanisms/ModMenuTypes.java
+++ src/main/java/com/ytbudka/mechanisms/ModMenuTypes.java
@@ -10,47 +10,13 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModMenuTypes {
     public static final DeferredRegister<MenuType<?>> MENUS =
-        
-# Обновляем MechanismsMod.java
-cat > src/main/java/com/ytbudka/mechanisms/MechanismsMod.java << 'EOF'
-package com.ytbudka.mechanisms;
-
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
-
-@Mod(MechanismsMod.MOD_ID)
-public class MechanismsMod {
-    public static final String MOD_ID = "mechanisms";
+        DeferredRegister.create(ForgeRegistries.MENU_TYPES, MechanismsMod.MOD_ID);
     
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = 
-        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
+    public static final RegistryObject<MenuType<CoalGeneratorMenu>> COAL_GENERATOR_MENU =
+        MENUS.register("coal_generator_menu", () ->
+            IForgeMenuType.create(CoalGeneratorMenu::new));
     
-    public static final RegistryObject<CreativeModeTab> MECHANISMS_TAB = CREATIVE_TABS.register("mechanisms_tab",
-        () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.mechanisms"))
-            .icon(() -> new ItemStack(ModBlocks.COAL_GENERATOR.get()))
-            .displayItems((params, output) -> {
-                output.accept(ModItems.COAL_GENERATOR_ITEM.get());
-            })
-            .build());
-    
-    public MechanismsMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
-        CREATIVE_TABS.register(modEventBus);
-        ModBlocks.register(modEventBus);
-        ModItems.register(modEventBus);
-        ModBlockEntities.register(modEventBus);
-        ModMenuTypes.register(modEventBus);
-        
-        MinecraftForge.EVENT_BUS.register(this);
+    public static void register(IEventBus eventBus) {
+        MENUS.register(eventBus);
     }
 }
