d2797964e4696e9beaa26a8dafbf88d2c7a3fb3e
7994406-stack <7994406@gmail.com>
2026-02-12 19:35:57 +0300

    Add GUI Screen and fix textures

---
+++ src/main/java/com/ytbudka/mechanisms/ClientSetup.java
@@ -0,0 +1,17 @@
+package com.ytbudka.mechanisms;
+
+import com.ytbudka.mechanisms.screen.CoalGeneratorScreen;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = MechanismsMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientSetup {
+    
+    @SubscribeEvent
+    public static void clientSetup(FMLClientSetupEvent event) {
+        MenuScreens.register(ModMenuTypes.COAL_GENERATOR_MENU.get(), CoalGeneratorScreen::new);
+    }
+}
