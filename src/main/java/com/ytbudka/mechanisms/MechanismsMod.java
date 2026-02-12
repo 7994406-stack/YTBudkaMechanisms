c0f7a0806993e32175a705260f726b2e79517c8a
7994406-stack <7994406@gmail.com>
2026-02-12 22:12:44 +0300

    Fix directory structure and build config

--- src/main/java/com/ytbudka/mechanisms/MechanismsMod.java
+++ src/main/java/com/ytbudka/mechanisms/MechanismsMod.java
@@ -1,43 +1,33 @@
 package com.ytbudka.mechanisms;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 
-@Mod(MechanismsMod.MOD_ID)
+@Mod("mechanisms")
 public class MechanismsMod {
     public static final String MOD_ID = "mechanisms";
     
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = 
-        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
     
     public static final RegistryObject<CreativeModeTab> MECHANISMS_TAB = CREATIVE_TABS.register("mechanisms_tab",
         () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.mechanisms"))
-            .icon(() -> new ItemStack(ModBlocks.COAL_GENERATOR.get()))
-            .displayItems((params, output) -> {
-                // Добавляем все блоки мода
-                output.accept(ModItems.COAL_GENERATOR_ITEM.get());
-            })
+            .icon(() -> new ItemStack(Items.REDSTONE))
             .build());
-    
+
     public MechanismsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
         CREATIVE_TABS.register(modEventBus);
-        ModBlocks.register(modEventBus);
-        ModItems.register(modEventBus);
-        ModBlockEntities.register(modEventBus);
-        ModMenuTypes.register(modEventBus);
-        
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
