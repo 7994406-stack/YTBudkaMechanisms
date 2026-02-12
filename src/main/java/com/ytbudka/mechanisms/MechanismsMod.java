4420add26c8ab495f9412ca29b4f08de5ce3a3b9
7994406-stack <7994406@gmail.com>
2026-02-12 22:38:51 +0300

    Complete mod rebuild - detection fix

--- src/main/java/com/ytbudka/mechanisms/MechanismsMod.java
+++ src/main/java/com/ytbudka/mechanisms/MechanismsMod.java
@@ -2,43 +2,49 @@ package com.ytbudka.mechanisms;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 @Mod(MechanismsMod.MOD_ID)
 public class MechanismsMod {
     public static final String MOD_ID = "mechanisms";
-    
-    // Регистры
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = 
-        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
-    
-    // Вкладка креатива
-    public static final RegistryObject<CreativeModeTab> MECHANISMS_TAB = CREATIVE_TABS.register("mechanisms_tab",
-        () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.mechanisms"))
-            .icon(() -> new ItemStack(Items.REDSTONE_BLOCK)) // Пока иконка - редстоун блок
-            .displayItems((params, output) -> {
-                output.accept(ModItems.COAL_GENERATOR_ITEM.get());
-            })
-            .build());
-    
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
+
+    public static final RegistryObject<Block> COAL_GENERATOR = BLOCKS.register("coal_generator",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
+
+    public static final RegistryObject<Item> COAL_GENERATOR_ITEM = ITEMS.register("coal_generator",
+            () -> new BlockItem(COAL_GENERATOR.get(), new Item.Properties()));
+
+    public static final RegistryObject<CreativeModeTab> MOD_TAB = CREATIVE_TABS.register("mechanisms_tab",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.mechanisms"))
+                    .icon(() -> new ItemStack(COAL_GENERATOR.get()))
+                    .displayItems((params, output) -> {
+                        output.accept(COAL_GENERATOR_ITEM.get());
+                    }).build());
+
     public MechanismsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
         CREATIVE_TABS.register(modEventBus);
-        
-        // Регистрируем остальные части (пока пусто, чтобы не крашило)
-        ModBlocks.register(modEventBus);
-        ModItems.register(modEventBus);
-        
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
